de67252d12d42731f72478ba97a5bfce25cc680e
PLS54 <plsigouin@gmail.com>
2018-09-03 11:11:20 -0400

    Initial commit

---
+++ Choix/ChoixApplication/Choix.Designer.cs
@@ -0,0 +1,62 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Choix
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBoxChoix = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // textBoxChoix
+            // 
+            this.textBoxChoix.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxChoix.Location = new System.Drawing.Point(32, 41);
+            this.textBoxChoix.Name = "textBoxChoix";
+            this.textBoxChoix.Size = new System.Drawing.Size(461, 29);
+            this.textBoxChoix.TabIndex = 0;
+            this.textBoxChoix.Text = "Choix";
+            // 
+            // Choix
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(543, 89);
+            this.Controls.Add(this.textBoxChoix);
+            this.Name = "Choix";
+            this.Text = "Choix";
+            this.Load += new System.EventHandler(this.Choix_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBoxChoix;
+    }
+}
+
---
+++ Choix/ChoixApplication/Choix.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Choix : Form
+    {
+
+        public Choix()
+        {
+            InitializeComponent();
+        }
+
+        private void Choix_Load(object sender, EventArgs e)
+        {
+
+            const string originalFile = @"files.txt";
+            const string doneFile = @"passées.txt";
+            //    
+            string[] originalFileContent = ReadTextFile(originalFile);
+            string[] doneContent = ReadTextFile(doneFile);
+            //
+            // Remove done from original list
+            //
+            Array.Sort(doneContent);
+            int drawCount = 0;
+            foreach(string s in originalFileContent) {
+                if (Array.BinarySearch(doneContent, s) < 0) {
+                    drawCount++;
+                }
+            }
+            if (drawCount == 0) {
+                textBoxChoix.Text = "Toutes finies";
+                return;
+            }
+            int count = 0;
+            String[] drawFrom = new String[drawCount];
+            foreach (string s in originalFileContent)
+            {
+                if (Array.BinarySearch(doneContent, s) < 0) {
+                    drawFrom[count++] = s;
+                }
+            }
+            //
+            // Do the draw
+            //
+            textBoxChoix.Text = drawFrom[DateTime.Now.Millisecond % drawFrom.Length];
+            File.AppendAllText(doneFile, doneContent.Length == 0 ? textBoxChoix.Text : Environment.NewLine + textBoxChoix.Text);
+        }
+
+        private static string[] ReadTextFile(string filename)
+        {
+            int counter = 0;
+            string line;
+            string[] theFileContent;
+            if (!File.Exists(filename)) {
+                return new String[0];
+            }
+            using (StreamReader file = new System.IO.StreamReader(filename)) {
+                while ((line = file.ReadLine()) != null) {
+                    counter++;
+                }
+            }
+            theFileContent = new String[counter];
+            counter = 0;
+            using (StreamReader file = new System.IO.StreamReader(filename)) {
+                while ((line = file.ReadLine()) != null) {
+                    theFileContent[counter++] = line;
+                }
+            }
+            return theFileContent;
+        }
+    }
+}
