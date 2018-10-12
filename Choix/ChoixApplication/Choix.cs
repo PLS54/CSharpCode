e779a9d33d08f1b335fe84b62b484c05b8e6aa40
PLS54 <plsigouin@gmail.com>
2018-10-12 15:36:19 -0400

    Format code

--- Choix/ChoixApplication/Choix.cs
+++ Choix/ChoixApplication/Choix.cs
@@ -29,19 +29,13 @@ namespace WindowsFormsApplication1
             string[] doneContent = ReadTextFile(doneFile);
             string[] originalFileContent = new String[0];
             //
-            foreach (string s in Directory.EnumerateDirectories(@"z:"))
-            {
-                if (!s.Equals(@"z:$RECYCLE.BIN", StringComparison.InvariantCulture) && !s.Equals(@"z:System Volume Information", StringComparison.InvariantCultureIgnoreCase))
-                {
-                    if (s.Equals(@"z:new", StringComparison.InvariantCultureIgnoreCase))
-                    {
-                        foreach (string sInner in Directory.EnumerateDirectories(@"z:new"))
-                        {
+            foreach (string s in Directory.EnumerateDirectories(@"z:")) {
+                if (!s.Equals(@"z:$RECYCLE.BIN", StringComparison.InvariantCulture) && !s.Equals(@"z:System Volume Information", StringComparison.InvariantCultureIgnoreCase)) {
+                    if (s.Equals(@"z:new", StringComparison.InvariantCultureIgnoreCase)) {
+                        foreach (string sInner in Directory.EnumerateDirectories(@"z:new")) {
                             AddToArray(ref originalFileContent, sInner);
                         }
-                    }
-                    else
-                    {
+                    } else {
                         AddToArray(ref originalFileContent, s);
                     }
                 }
@@ -51,10 +45,8 @@ namespace WindowsFormsApplication1
             //
             Array.Sort(doneContent);
             String[] drawFrom = new String[0];
-            foreach (string s in originalFileContent)
-            {
-                if (Array.BinarySearch(doneContent, s) < 0)
-                {
+            foreach (string s in originalFileContent) {
+                if (Array.BinarySearch(doneContent, s) < 0) {
                     Array.Resize<String>(ref drawFrom, drawFrom.Length + 1);
                     drawFrom[drawFrom.Length - 1] = s;
                 }
@@ -63,45 +55,36 @@ namespace WindowsFormsApplication1
             // Do the draw
             //
             textBoxChoix.ReadOnly = true;
-            if (drawFrom.Length == 0)
-            {
+            if (drawFrom.Length == 0) {
                 textBoxChoix.Text = "Pas de fichier source";
                 return;
             }
             textBoxChoix.Text = Draw(drawFrom);
             File.AppendAllText(doneFile, doneContent.Length == 0 ? textBoxChoix.Text : Environment.NewLine + textBoxChoix.Text);
             String dirName;
-            if (textBoxChoix.Text.StartsWith("new - "))
-            {
+            if (textBoxChoix.Text.StartsWith("new - ")) {
                 dirName = String.Format("{0}{1}", @"z:\New\", textBoxChoix.Text.Substring(6));
-            }
-            else
-            {
+            } else {
                 dirName = String.Format("{0}{1}", @"z:\", textBoxChoix.Text);
             }
             String[] drawFrom1 = new String[0];
             String[] drawFrom2 = new String[0];
             String[] drawFrom3 = new String[0];
-            foreach (string s in Directory.EnumerateFiles(dirName))
-            {
-                if (Path.GetFileName(s).StartsWith("1", StringComparison.InvariantCultureIgnoreCase))
-                {
+            foreach (string s in Directory.EnumerateFiles(dirName)) {
+                if (Path.GetFileName(s).StartsWith("1", StringComparison.InvariantCultureIgnoreCase)) {
                     AddToArray(ref drawFrom1, Path.GetFileName(s), false);
                 }
-                if (Path.GetFileName(s).StartsWith("2", StringComparison.InvariantCultureIgnoreCase))
-                {
+                if (Path.GetFileName(s).StartsWith("2", StringComparison.InvariantCultureIgnoreCase)) {
                     AddToArray(ref drawFrom2, Path.GetFileName(s), false);
                 }
-                if (Path.GetFileName(s).StartsWith("3", StringComparison.InvariantCultureIgnoreCase))
-                {
+                if (Path.GetFileName(s).StartsWith("3", StringComparison.InvariantCultureIgnoreCase)) {
                     AddToArray(ref drawFrom3, Path.GetFileName(s), false);
                 }
             }
             textBoxChoix.Text = String.Format("{0} -> {1} | {2} | {3}", textBoxChoix.Text, Draw(drawFrom1), Draw(drawFrom2), Draw(drawFrom3));
             textBoxChoix.SelectionStart = textBoxChoix.Text.Length;
             textBoxChoix.DeselectAll();
-            if (drawFrom.Length == 1)
-            {
+            if (drawFrom.Length == 1) {
                 textBoxChoix.BackColor = Color.White;
                 textBoxChoix.ForeColor = Color.Red;
                 File.Delete(doneFile);
@@ -126,7 +109,7 @@ namespace WindowsFormsApplication1
             using (StreamReader file = new System.IO.StreamReader(filename)) {
                 while ((line = file.ReadLine()) != null) {
                     Array.Resize<String>(ref theFileContent, theFileContent.Length + 1);
-                    theFileContent[theFileContent.Length -1] = line;
+                    theFileContent[theFileContent.Length - 1] = line;
                 }
             }
             return theFileContent;
